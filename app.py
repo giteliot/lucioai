8af0c0e9e550695a625fdc61a3c9ec1b43a99a85
eliot <echinellato@cuebiq.com>
2023-03-06 22:26:52 +0100

    basic inerface

--- app.py
+++ app.py
@@ -4,13 +4,19 @@ import torch
 app = Flask(__name__)
 model = None  # we'll load the model later
 
+
 def preprocess_input(input_data):
     # Implement this function to preprocess the input data (e.g. convert it to a tensor)
     return torch.tensor([float(input_data)])
 
+
 def postprocess_output(output_tensor):
     # Implement this function to postprocess the output data (e.g. convert it to a string)
     return str(output_tensor.item())
+
+
+def process_input(input_data):
+    return "BAU"
 
 
 # Define the route for the index page
@@ -20,13 +26,11 @@ def index():
 
 
 # Define the route for the model prediction
-@app.route('/predict', methods=['POST'])
+@app.route('/command', methods=['POST'])
 def predict():
     input_data = request.form['input']
-    input_tensor = preprocess_input(input_data)
-    output_tensor = model(input_tensor)
-    output_data = postprocess_output(output_tensor)
-    return render_template('result.html', output=output_data)
+    output_action = process_input(input_data)
+    return render_template('index.html', input_text=input_data, action=output_action)
 
 
 if __name__ == '__main__':
